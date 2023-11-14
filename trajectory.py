598a5d2ea524236159318d85cf992920778c076e
AlejandroFloresM02 <A01634167@itesm.mx>
2023-11-13 20:55:46 -0600

    Added path following and voltage logging

--- trajectory.py
+++ trajectory.py
@@ -1,22 +1,21 @@
 import numpy as np
 
-def gen_path(width, length, height, step):
+def gen_path(width, length, height, step, padding):
     inverse = False
-    max_width = width//step * step
-    print(max_width)
-    for i in np.arange(0,length,step):
+    #print(max_width)
+    for i in np.arange(padding,length-padding+0.01,step):
         match inverse:
             case False:
-                for j in np.arange(step,width + 0.01,step):
-                    print("[%.3f,%.3f,%.3f]" % (j,i,height), end = ";")
+                for j in np.arange(padding,width-padding + 0.01,step):
+                    print("(%.3f,%.3f,%.3f,0)" % (i,j,height), end = ",\n")
                 inverse = True
             case True:
-                for j in np.arange(max_width,step - 0.01,-step):
-                    print("[%.3f,%.3f,%.3f]" % (j,i,height), end = ";")
+                for j in np.arange(width - padding,padding-0.01,-step):
+                    print("(%.3f,%.3f,%.3f,0)" % (i,j,height), end = ",\n")
                 inverse = False
 
     return
 
 
 if __name__ =="__main__":
-    gen_path(2.1,5.32,1.8,.3) #values in mts
+    gen_path(2.2,5.3,1.5,0.6,0.5) #values in mts
